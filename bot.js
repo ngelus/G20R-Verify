553c18d8b17a989042dfac219d994260c67ea6e9
ngelus <nc@sippicom.local>
2023-02-08 10:32:58 +0100

    logging and first command

--- bot.js
+++ bot.js
@@ -1,4 +1,29 @@
 /* ### SETUP AND CONFIGURATION ### */
+const fs = require('fs');
+const path = require('path');
+
+const winston = require('winston');
+
+const loggingFormat = winston.format.printf(
+  ({ level, message, label, timestamp }) => {
+    return `${timestamp} [${level}]: ${message}`;
+  }
+);
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    loggingFormat
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -7,7 +32,7 @@ const TOKEN = process.env.TOKEN;
 
 var config = require('./config.json');
 
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -16,12 +41,34 @@ const client = new Client({
   ],
 });
 
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith('.js'));
+
 /* ### COMMANDS AND CODE ### */
 
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    logger.warn(
+      `The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+}
+
 client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
+  logger.info(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  logger.log(interaction);
 });
 
 /* ### LOGIN ### */
-
-client.login(TOKEN);
+client.login(TOKEN.toString());
